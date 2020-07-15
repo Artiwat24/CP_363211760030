5e3de569c450c56c041844b7e826067444e591a2
Film Artiwat <artiwat.a@rmutsvmail.com>
2020-07-15 14:56:14 +0700

    add  more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,8 +5,10 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
-
-
+        System.out.println("Artiwat Akarapngsakul");
+        System.out.println("S363211760030");
+        System.out.println("เทคโนโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
     }
 
 }
